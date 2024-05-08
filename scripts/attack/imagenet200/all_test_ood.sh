87e8fad8ed89fa3728104bb94e20c99cdb938d15
Peter Lorenz <peter.lorenz@itwm.fraunhofer.de>
2024-05-08 21:36:30 +0200

    .

--- scripts/attack/imagenet200/all_test_ood.sh
+++ scripts/attack/imagenet200/all_test_ood.sh
@@ -5,14 +5,14 @@ python scripts/attack_ood.py \
    --id-data imagenet200 \
    --root ./results/imagenet200_resnet18_224x224_base_e90_lr0.1_default \
    --att pgd \
-   --eps "4/255"
+   --eps "4/255" \
    --batch-size 64
 
 python scripts/attack_ood.py \
    --id-data imagenet200 \
    --root ./results/imagenet200_resnet18_224x224_base_e90_lr0.1_default \
    --att fgsm \
-   --eps "4/255"
+   --eps "4/255" \
    --batch-size 64
 
 python scripts/attack_ood.py \
@@ -27,5 +27,5 @@ python scripts/attack_ood.py \
    --id-data imagenet200 \
    --root ./results/imagenet200_resnet18_224x224_base_e90_lr0.1_default \
    --att mpgd \
-   --eps "1"
+   --eps "1" \
    --batch-size 64
